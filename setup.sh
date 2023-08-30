12737908bb205d49a1b685b0d153f06b848da3bc
Devin-Yeung <53309384+Devin-Yeung@users.noreply.github.com>
2023-08-30 15:53:14 +0000

    chore: make script quiet

--- setup.sh
+++ setup.sh
@@ -8,8 +8,8 @@ mkdir -p ~/.codespace/bin
 mkdir -p ~/.config
 
 # setup neovim
-wget https://github.com/neovim/neovim/releases/download/v0.9.1/nvim-linux64.tar.gz --output-document ~/install/nvim-linux64.tar.gz
-tar -xvf ~/install/nvim-linux64.tar.gz -C ~/.codespace/bin
+wget -q https://github.com/neovim/neovim/releases/download/v0.9.1/nvim-linux64.tar.gz --output-document ~/install/nvim-linux64.tar.gz
+tar -xf ~/install/nvim-linux64.tar.gz -C ~/.codespace/bin
 sudo ln -s ~/.codespace/bin/nvim-linux64/bin/nvim /usr/bin/nvim
 
 # setup nvchad
@@ -28,14 +28,14 @@ mkdir -p ~/.zsh/antigen
 curl -L git.io/antigen > ~/.zsh/antigen/antigen.zsh
 
 # zoxide
-wget https://github.com/ajeetdsouza/zoxide/releases/download/v0.9.2/zoxide-0.9.2-x86_64-unknown-linux-musl.tar.gz -O ~/install/zoxide.tar.gz
+wget -q https://github.com/ajeetdsouza/zoxide/releases/download/v0.9.2/zoxide-0.9.2-x86_64-unknown-linux-musl.tar.gz -O ~/install/zoxide.tar.gz
 mkdir -p ~/.codespace/bin/zoxide
-tar -xvf ~/install/zoxide.tar.gz -C ~/.codespace/bin/zoxide
+tar -xf ~/install/zoxide.tar.gz -C ~/.codespace/bin/zoxide
 sudo ln -s ~/.codespace/bin/zoxide/zoxide /usr/bin/zoxide
 
 # starship
-wget https://github.com/starship/starship/releases/download/v1.16.0/starship-x86_64-unknown-linux-musl.tar.gz -O ~/install/starship.tar.gz
-tar -xvf ~/install/starship.tar.gz -C ~/.codespace/bin
+wget -q https://github.com/starship/starship/releases/download/v1.16.0/starship-x86_64-unknown-linux-musl.tar.gz -O ~/install/starship.tar.gz
+tar -xf ~/install/starship.tar.gz -C ~/.codespace/bin
 sudo ln -s ~/.codespace/bin/starship /usr/bin/starship
 
 # git config
--- utils.sh
+++ utils.sh
@@ -2,23 +2,23 @@
 mkdir -p ~/install
 
 # ripgrep
-wget https://github.com/BurntSushi/ripgrep/releases/download/13.0.0/ripgrep-13.0.0-x86_64-unknown-linux-musl.tar.gz -O ~/install/ripgrep.tar.gz
-tar -xvf ~/install/ripgrep.tar.gz -C ~/.codespace/bin --transform s/ripgrep-.*-x86_64-unknown-linux-musl/ripgrep/
-sudo ln -s ~/.codespace/bin/ripgrep/rg /usr/bin/rg
+wget -q https://github.com/BurntSushi/ripgrep/releases/download/13.0.0/ripgrep-13.0.0-x86_64-unknown-linux-musl.tar.gz -O ~/install/ripgrep.tar.gz
+tar -xf ~/install/ripgrep.tar.gz -C ~/.codespace/bin --transform s/ripgrep-.*-x86_64-unknown-linux-musl/ripgrep/
+sudo ln -sf ~/.codespace/bin/ripgrep/rg /usr/bin/rg
 
 # exa
-wget 'https://github.com/ogham/exa/releases/download/v0.10.1/exa-linux-x86_64-musl-v0.10.1.zip' -O ~/install/exa.zip
+wget -q 'https://github.com/ogham/exa/releases/download/v0.10.1/exa-linux-x86_64-musl-v0.10.1.zip' -O ~/install/exa.zip
 mkdir -p ~/.codespace/bin/exa
 unzip ~/install/exa.zip -f -d ~/.codespace/bin/exa
-sudo ln -s ~/.codespace/bin/exa/bin/exa /usr/bin/exa
+sudo ln -sf ~/.codespace/bin/exa/bin/exa /usr/bin/exa
 
 # fd
-wget 'https://github.com/sharkdp/fd/releases/download/v8.7.0/fd-v8.7.0-x86_64-unknown-linux-musl.tar.gz' -O ~/install/fd.tar.gz
-tar -xvf ~/install/fd.tar.gz -C ~/.codespace/bin --transform s/fd-.*-unknown-linux-musl/fd/
-sudo ln -s ~/.codespace/bin/fd/fd /usr/bin/fd
+wget -q 'https://github.com/sharkdp/fd/releases/download/v8.7.0/fd-v8.7.0-x86_64-unknown-linux-musl.tar.gz' -O ~/install/fd.tar.gz
+tar -xf ~/install/fd.tar.gz -C ~/.codespace/bin --transform s/fd-.*-unknown-linux-musl/fd/
+sudo ln -sf ~/.codespace/bin/fd/fd /usr/bin/fd
 
 # difft
-wget 'https://github.com/Wilfred/difftastic/releases/download/0.50.0/difft-x86_64-unknown-linux-gnu.tar.gz' -O ~/install/difft.tar.gz
+wget -q 'https://github.com/Wilfred/difftastic/releases/download/0.50.0/difft-x86_64-unknown-linux-gnu.tar.gz' -O ~/install/difft.tar.gz
 mkdir -p ~/.codespace/bin/difft
-tar -xvf ~/install/difft.tar.gz -C ~/.codespace/bin/difft
-sudo ln -s ~/.codespace/bin/difft/difft /usr/bin/difft
+tar -xf ~/install/difft.tar.gz -C ~/.codespace/bin/difft
+sudo ln -sf ~/.codespace/bin/difft/difft /usr/bin/difft
