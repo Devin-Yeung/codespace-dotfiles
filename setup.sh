71f7f053d348c63338a4bbdbca839b3872fc4db8
Devin-Yeung <53309384+Devin-Yeung@users.noreply.github.com>
2023-08-31 06:12:52 +0000

    refactor: move all binary installation to one script

--- setup.sh
+++ setup.sh
@@ -7,11 +7,6 @@ mkdir -p ~/.codespace/bin
 # config dir
 mkdir -p ~/.config
 
-# setup neovim
-wget -q https://github.com/neovim/neovim/releases/download/v0.9.1/nvim-linux64.tar.gz --output-document ~/install/nvim-linux64.tar.gz
-tar -xf ~/install/nvim-linux64.tar.gz -C ~/.codespace/bin
-sudo ln -s ~/.codespace/bin/nvim-linux64/bin/nvim /usr/bin/nvim
-
 # setup nvchad
 git clone https://github.com/NvChad/NvChad.git --depth=1 ~/.config/nvim
 
@@ -26,17 +21,6 @@ cp .zshrc ~/.zshrc
 # antigen
 mkdir -p ~/.zsh/antigen
 curl -L git.io/antigen > ~/.zsh/antigen/antigen.zsh
-
-# zoxide
-wget -q https://github.com/ajeetdsouza/zoxide/releases/download/v0.9.2/zoxide-0.9.2-x86_64-unknown-linux-musl.tar.gz -O ~/install/zoxide.tar.gz
-mkdir -p ~/.codespace/bin/zoxide
-tar -xf ~/install/zoxide.tar.gz -C ~/.codespace/bin/zoxide
-sudo ln -s ~/.codespace/bin/zoxide/zoxide /usr/bin/zoxide
-
-# starship
-wget -q https://github.com/starship/starship/releases/download/v1.16.0/starship-x86_64-unknown-linux-musl.tar.gz -O ~/install/starship.tar.gz
-tar -xf ~/install/starship.tar.gz -C ~/.codespace/bin
-sudo ln -s ~/.codespace/bin/starship /usr/bin/starship
 
 # git config
 cp .gitconfig ~/.gitconfig
--- utils.sh
+++ utils.sh
@@ -1,6 +1,11 @@
 # install some useful utils
 mkdir -p ~/install
 
+# neovim
+wget -q https://github.com/neovim/neovim/releases/download/v0.9.1/nvim-linux64.tar.gz -O ~/install/nvim-linux64.tar.gz
+tar -xf ~/install/nvim-linux64.tar.gz -C ~/.codespace/bin
+sudo ln -s ~/.codespace/bin/nvim-linux64/bin/nvim /usr/bin/nvim
+
 # ripgrep
 wget -q https://github.com/BurntSushi/ripgrep/releases/download/13.0.0/ripgrep-13.0.0-x86_64-unknown-linux-musl.tar.gz -O ~/install/ripgrep.tar.gz
 tar -xf ~/install/ripgrep.tar.gz -C ~/.codespace/bin --transform s/ripgrep-.*-x86_64-unknown-linux-musl/ripgrep/
@@ -22,3 +27,14 @@ wget -q 'https://github.com/Wilfred/difftastic/releases/download/0.50.0/difft-x8
 mkdir -p ~/.codespace/bin/difft
 tar -xf ~/install/difft.tar.gz -C ~/.codespace/bin/difft
 sudo ln -sf ~/.codespace/bin/difft/difft /usr/bin/difft
+
+# zoxide
+wget -q https://github.com/ajeetdsouza/zoxide/releases/download/v0.9.2/zoxide-0.9.2-x86_64-unknown-linux-musl.tar.gz -O ~/install/zoxide.tar.gz
+mkdir -p ~/.codespace/bin/zoxide
+tar -xf ~/install/zoxide.tar.gz -C ~/.codespace/bin/zoxide
+sudo ln -s ~/.codespace/bin/zoxide/zoxide /usr/bin/zoxide
+
+# starship
+wget -q https://github.com/starship/starship/releases/download/v1.16.0/starship-x86_64-unknown-linux-musl.tar.gz -O ~/install/starship.tar.gz
+tar -xf ~/install/starship.tar.gz -C ~/.codespace/bin
+sudo ln -s ~/.codespace/bin/starship /usr/bin/starship
