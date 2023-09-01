05abadf0d97d45e0f5bb8ae1e2956746670bec2a
Devin-Yeung <53309384+Devin-Yeung@users.noreply.github.com>
2023-09-01 16:18:32 +0800

    feat: use better way to install utils (#4)

--- utils.sh
+++ utils.sh
@@ -1,42 +1,51 @@
-#!/bin/sh
-# install some useful utils
-mkdir -p ~/install
+#!/bin/bash
 
-# neovim
-wget -q https://github.com/neovim/neovim/releases/download/v0.9.1/nvim-linux64.tar.gz -O ~/install/nvim-linux64.tar.gz
-tar -xf ~/install/nvim-linux64.tar.gz -C ~/.codespace/bin
-sudo ln -s ~/.codespace/bin/nvim-linux64/bin/nvim /usr/bin/nvim
+extension() {
+    # input url
+    local url=${1//\"/} # remove the quote if there are
+    # List of valid archive extensions
+    local valid=("zip" "tar.gz")
 
-# ripgrep
-wget -q https://github.com/BurntSushi/ripgrep/releases/download/13.0.0/ripgrep-13.0.0-x86_64-unknown-linux-musl.tar.gz -O ~/install/ripgrep.tar.gz
-tar -xf ~/install/ripgrep.tar.gz -C ~/.codespace/bin --transform s/ripgrep-.*-x86_64-unknown-linux-musl/ripgrep/
-sudo ln -sf ~/.codespace/bin/ripgrep/rg /usr/bin/rg
+    for ext in "${valid[@]}"; do
+        if [[ "$url" =~ \.$ext$ ]]; then
+            echo "$ext"
+            return
+        fi
+    done
 
-# exa
-wget -q 'https://github.com/ogham/exa/releases/download/v0.10.1/exa-linux-x86_64-musl-v0.10.1.zip' -O ~/install/exa.zip
-mkdir -p ~/.codespace/bin/exa
-unzip -o -q ~/install/exa.zip -d ~/.codespace/bin/exa
-sudo ln -sf ~/.codespace/bin/exa/bin/exa /usr/bin/exa
+    echo ""
+}
 
-# fd
-wget -q 'https://github.com/sharkdp/fd/releases/download/v8.7.0/fd-v8.7.0-x86_64-unknown-linux-musl.tar.gz' -O ~/install/fd.tar.gz
-tar -xf ~/install/fd.tar.gz -C ~/.codespace/bin --transform s/fd-.*-unknown-linux-musl/fd/
-sudo ln -sf ~/.codespace/bin/fd/fd /usr/bin/fd
+cat utils.json | jq '.[]' -c | while IFS= read -r item; do
+    # see https://stackoverflow.com/questions/44656515/how-to-remove-double-quotes-in-jq-output-for-parsing-json-files-in-bash
+    url=$(jq -r '.url' <<< $item)
+    name=$(jq -r '.name' <<< $item)
+    ext=$(extension $url)
+    wget -q $url -O ~/install/"$name.$ext"
 
-# difft
-wget -q 'https://github.com/Wilfred/difftastic/releases/download/0.50.0/difft-x86_64-unknown-linux-gnu.tar.gz' -O ~/install/difft.tar.gz
-mkdir -p ~/.codespace/bin/difft
-tar -xf ~/install/difft.tar.gz -C ~/.codespace/bin/difft
-sudo ln -sf ~/.codespace/bin/difft/difft /usr/bin/difft
+    mkdir -p ~/.codespace/bin/"$name"
+    # unzip base on ext
+    if [ "$ext" = "tar.gz" ]; then
+        tar -xf ~/install/"$name.$ext" -C ~/.codespace/bin/"$name"
+    elif [ "$ext" = "zip" ]; then
+        unzip -o -q ~/install/"$name.$ext" -d ~/.codespace/bin/"$name"
+    fi
 
-# zoxide
-wget -q https://github.com/ajeetdsouza/zoxide/releases/download/v0.9.2/zoxide-0.9.2-x86_64-unknown-linux-musl.tar.gz -O ~/install/zoxide.tar.gz
-mkdir -p ~/.codespace/bin/zoxide
-tar -xf ~/install/zoxide.tar.gz -C ~/.codespace/bin/zoxide
-sudo ln -s ~/.codespace/bin/zoxide/zoxide /usr/bin/zoxide
+    # don't peel if field is missing
+    pattern=$(jq -r '.peel' <<< $item)
+    if [ "$pattern" != "null" ]; then
+        from=$(find ~/.codespace/bin/$name -maxdepth 1 -type d -name $pattern)
+        to="$(echo ~)"/.codespace/bin/$name
+        # check if it is the same name before peel
+        if [ ! "$from" = "$to" ]; then
+            echo "peel $from/* to $to"
+            mv $from/* $to
+            rm -r $from
+        fi
+    fi
 
-# starship
-wget -q https://github.com/starship/starship/releases/download/v1.16.0/starship-x86_64-unknown-linux-musl.tar.gz -O ~/install/starship.tar.gz
-mkdir -p ~/.codespace/bin/starship
-tar -xf ~/install/starship.tar.gz -C ~/.codespace/bin/starship
-sudo ln -s ~/.codespace/bin/starship/starship /usr/bin/starship
+    path=$(jq -r '.path' <<< $item)
+    bin=$(basename $path)
+    echo "link ~/.codespace/bin/$name/$path to /usr/bin/$bin"
+    sudo ln -sf ~/.codespace/bin/"$name/$path" /usr/bin/"$bin"
+done
