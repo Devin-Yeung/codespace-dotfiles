66c7896ee7d48d7800e75bbd5dc465602b769b6a
Devin-Yeung <53309384+Devin-Yeung@users.noreply.github.com>
2024-02-04 18:02:16 +0000

    chore: fmt

--- nvchad/custom/configs/overrides.lua
+++ nvchad/custom/configs/overrides.lua
@@ -1,26 +1,26 @@
 local M = {}
 
 M.treesitter = {
-  ensure_installed = {
-    "c",
-    "vim",
-    "lua",
-    "rust",
-    "toml",
-    "markdown",
-    "markdown_inline",
-    -- "html",
-    -- "css",
-    -- "javascript",
-    -- "typescript",
-    -- "tsx",
-  },
-  indent = {
-    enable = true,
-    -- disable = {
-    --   "python"
-    -- },
-  },
+	ensure_installed = {
+		"c",
+		"vim",
+		"lua",
+		"rust",
+		"toml",
+		"markdown",
+		"markdown_inline",
+		-- "html",
+		-- "css",
+		-- "javascript",
+		-- "typescript",
+		-- "tsx",
+	},
+	indent = {
+		enable = true,
+		-- disable = {
+		--   "python"
+		-- },
+	},
 }
 
 M.mason = {
@@ -48,18 +48,18 @@ M.mason = {
 
 -- git support in nvimtree
 M.nvimtree = {
-  git = {
-    enable = true,
-  },
+	git = {
+		enable = true,
+	},
 
-  renderer = {
-    highlight_git = true,
-    icons = {
-      show = {
-        git = true,
-      },
-    },
-  },
+	renderer = {
+		highlight_git = true,
+		icons = {
+			show = {
+				git = true,
+			},
+		},
+	},
 }
 
 return M
