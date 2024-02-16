af13f1d908af08e66018f7bc681e457c3e87a5c0
Devin Yeung <devin.cg.yeung.dev@outlook.com>
2024-02-16 22:06:37 +0800

    refactor: refactor zsh and git installation

--- oneline.sh
+++ oneline.sh
@@ -11,3 +11,22 @@ sudo apt install tmux -y
 # tmux config
 cp .tmux.conf ~/.tmux.conf
 # <<<<< tmux <<<<<
+
+
+# >>>>> zsh >>>>>
+# git config
+cp .gitconfig ~/.gitconfig
+# <<<<< git <<<<<
+
+
+# >>>>> zsh >>>>>
+# use zsh as default
+# see https://docs.github.com/en/codespaces/customizing-your-codespace/changing-the-shell-in-a-codespace#setting-the-default-shell-over-ssh
+sudo chsh "$(id -un)" --shell /usr/bin/zsh
+# zsh config
+cp .zshrc ~/.zshrc
+# antigen
+mkdir -p ~/.zsh/antigen
+curl -L git.io/antigen > ~/.zsh/antigen/antigen.zsh
+# <<<<< zsh <<<<<
+
--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+
 # bootstrap
 sh preinstall.sh
 
@@ -7,23 +8,6 @@ bash utils.sh
 
 # install the utils that not directly fetch from github release
 bash oneline.sh
-
-
-# zsh setup
-
-# use zsh as default
-# see https://docs.github.com/en/codespaces/customizing-your-codespace/changing-the-shell-in-a-codespace#setting-the-default-shell-over-ssh
-sudo chsh "$(id -un)" --shell /usr/bin/zsh
-
-# config file
-cp .zshrc ~/.zshrc
-
-# antigen
-mkdir -p ~/.zsh/antigen
-curl -L git.io/antigen > ~/.zsh/antigen/antigen.zsh
-
-# git config
-cp .gitconfig ~/.gitconfig
 
 # man pages
 for section in 1 3 5 7 8; do
