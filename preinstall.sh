ef6b6c6966e44cb727a8cdb83f845f29315804d0
Devin Yeung <devin.cg.yeung.dev@outlook.com>
2024-02-23 00:17:18 +0800

    chore: make sure apt prompt is confirmed

--- preinstall.sh
+++ preinstall.sh
@@ -10,8 +10,8 @@ mkdir -p ~/.codespace/bin
 mkdir -p ~/.config
 
 # apt update
-sudo apt upgrade
-sudo apt update
+sudo apt upgrade -y
+sudo apt update -y
 
 # preinstall jq
 sudo wget https://github.com/jqlang/jq/releases/download/jq-1.6/jq-linux64 -O /usr/bin/jq
