3401da045cfa897adacaa217b4c9707bf2324be5
Devin-Yeung <53309384+Devin-Yeung@users.noreply.github.com>
2024-11-27 02:31:46 +0800

    chore: I dont have time to fix it yet and it does not work now.

--- nvchad/custom/plugins.lua
+++ nvchad/custom/plugins.lua
@@ -71,20 +71,6 @@ local plugins = {
   -- avoid CPU stucking in M3 mac
   {
     "lukas-reineke/indent-blankline.nvim",
-    event = "User FilePost",
-    opts = {
-      indent = { char = "│", highlight = "IblChar" },
-      scope = { char = "│", highlight = "IblScopeChar" },
-    },
-    config = function(_, opts)
-      dofile(vim.g.base46_cache .. "blankline")
-
-      local hooks = require("ibl.hooks")
-      hooks.register(hooks.type.WHITESPACE, hooks.builtin.hide_first_space_indent_level)
-      require("ibl").setup(opts)
-
-      dofile(vim.g.base46_cache .. "blankline")
-    end,
     enabled = false,
   },
 
