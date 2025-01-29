e9c7fe46a0776468b21ff959b4e84dd1b15652ea
Devin-Yeung <53309384+Devin-Yeung@users.noreply.github.com>
2025-01-29 05:57:15 +0000

    feat: setup a naive nix config

---
+++ flake.nix
@@ -0,0 +1,72 @@
+{
+  description = "CodeSpace package collections";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+
+        # Create custom package sets
+        languageDevTools = with pkgs; {
+          rust = [
+            rustc
+            cargo
+            rust-analyzer
+          ];
+        };
+
+        # Common development tools
+        common = with pkgs; [
+          git
+          ripgrep
+          fd
+          jq
+          btop
+          tmux
+        ];
+
+      in
+      {
+        # Packages that can be installed individually
+        packages = {
+          # Development environments
+          rust = pkgs.buildEnv {
+            name = "rust";
+            paths = languageDevTools.rust ++ common;
+          };
+
+          # Standalone tools and custom packages
+          tools = pkgs.buildEnv {
+            name = "common-tools";
+            paths = common;
+          };
+
+          # Default package
+          default = self.packages.${system}.tools;
+        };
+
+        # Development shells for different environments
+        devShells = {
+          default = pkgs.mkShell {
+            buildInputs = common;
+          };
+
+          rust = pkgs.mkShell {
+            buildInputs = languageDevTools.rust ++ common;
+          };
+        };
+      }
+    );
+}
