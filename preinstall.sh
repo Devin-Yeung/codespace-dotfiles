ac18ea1eda8efae3b24caf033c2df471ffda2ea5
Devin-Yeung <53309384+Devin-Yeung@users.noreply.github.com>
2025-01-29 10:16:00 +0000

    chore: make dotfiles easier to find

--- preinstall.sh
+++ preinstall.sh
@@ -11,11 +11,15 @@ sudo chown -R $(id -u):$(id -u) /nix
 echo "Caching pkgs ... ($(id -u))"
 nix build --no-link .#full
 
+mkdir $HOME/.codespace
+# link config
+ln -s /workspaces/.codespaces/.persistedshare/dotfiles $HOME/.codespace/config
+
 # setup tmpdir
 mkdir -p ~/install
 
 # all binaries are installed into ~/.codespace/bin
-mkdir -p ~/.codespace/bin
+mkdir -p $HOME/.codespace/bin
 
 # config dir
 mkdir -p ~/.config
