803f3105649b2fa9c88733bcad5af8720de76825
Devin Yeung <devin.cg.yeung.dev@outlook.com>
2025-02-10 14:22:15 +0800

    feat: ocaml lsp support

--- nvchad/custom/configs/conform.lua
+++ nvchad/custom/configs/conform.lua
@@ -6,6 +6,7 @@ local options = {
     lua = { "stylua" },
     rust = { "rustfmt" },
     python = { "ruff_format" },
+    ocaml = { "ocamlformat" },
 
     json = { "prettier" },
     javascript = { "prettier" },
@@ -18,11 +19,11 @@ local options = {
   -- adding same formatter for multiple filetypes can look too much work for some
   -- instead of the above code you could just use a loop! the config is just a table after all!
 
-  -- format_on_save = {
-  --   -- These options will be passed to conform.format()
-  --   timeout_ms = 500,
-  --   lsp_fallback = true,
-  -- },
+  format_on_save = {
+    -- These options will be passed to conform.format()
+    timeout_ms = 500,
+    lsp_fallback = true,
+  },
 }
 
 require("conform").setup(options)
